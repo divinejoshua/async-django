1c9c60e1a189b2f7fffc73aed5ae1b07c749749d
divinejoshua <divineuche399@gmail.com>
2024-07-17 20:15:24 +0100

    Added heavy function

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import time
+from asgiref.sync import sync_to_async
 # Create your views here.
 
 
@@ -10,6 +11,18 @@ class BlogView(APIView):
     # Async function
     def get(self, request, format=None):
         data = {}
+        start_time = time.time()
+
         time.sleep(5)
+        total_time = (time.time() - start_time)
+
         data['name'] = "Divine"
+        data['total_time'] = total_time
         return Response(data,  status=status.HTTP_200_OK)
+
+
+
+# Heavy Function representation
+def mock_heavy_function(operation_time):
+    time.sleep(operation_time)
+    return True
